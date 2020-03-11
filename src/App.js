d053ea64cb857da80970628ea15f16dfa795367f
Shoozle <sean@seanmurtagh.uk>
2020-03-11 23:06:34 +0000

    Navigation started

--- src/App.js
+++ src/App.js
@@ -2,35 +2,44 @@ import React, { Component } from 'react';
 // import Navigation from './components/Navigation/Navigation';
 import DrawingCanvas from './components/DrawingCanvas/DrawingCanvas';
 import Toolbox from './components/Toolbox/Toolbox';
+import Navigation from './components/Navigation/Navigation';
+import { ChromePicker } from 'react-color';
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.brushcolor = 'red';
+        this.state = {
+            background: '#fff',
+        }
     }
 
-    componentDidMount(){
-        console.log('READY');
-        this.brushcolor = 'blue';
-        console.log(this.props.brushcolor);
+    componentDidMount() {
     }
 
-    getBrushColor(color){
-        console.log(color);
+    handleChangeComplete = (color) => {
+        this.setState({ background: color.hex });
+        console.log(this.state.background);
     }
 
     render() {
         return (
             <div className="App">
-                <Toolbox/>
-                {console.log(color)}
-                <DrawingCanvas />
-                <h1>{this.brushcolor}</h1>
-                {console.log(this.brushcolor)}
+            <Navigation />
+                <div className="toolbox">
+                <ChromePicker
+                    color={this.state.background}
+                    onChange={this.handleChangeComplete}
+                />
+                </div>
+                <DrawingCanvas brushcolor={this.state.background} />
+                <h1>{this.state.background}</h1>
             </div>
         );
     }
 }
+
+//<Toolbox brushcolor={this.brushcolor}/>
+//IS HOW WE PASS PROPS DOWN TO CHILDREN, AND THEN CALL THEM VIA THIS.PROPS.BRUSHCOLO IN OTHER COMPONENT
 
 export default App;
--- src/components/Toolbox/Toolbox.js
+++ src/components/Toolbox/Toolbox.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ChromePicker } from 'react-color';
 import './Toolbox.css';
 
 class Toolbox extends React.Component {
@@ -18,12 +17,8 @@ class Toolbox extends React.Component {
 
     render() {
         return (
-            <div className='toolbox' color={this.brushcolor}>
-                <ChromePicker
-                    color={this.state.background}
-                    onChange={this.handleChangeComplete}
-                />
-                <h1>{this.props.brushcolor}</h1>
+            <div className='toolbox'>
+
             </div>
         );
     }

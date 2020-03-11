0f8d8b2a5b23ef8629fe5f1b9af2c691b3345b54
Shoozle <sean@seanmurtagh.uk>
2020-03-11 23:09:12 +0000

    Nav CSS

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-
+        <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <p className='pointer pa2'>Browse</p>
+            <p className='pointer pa2'>Sign Out</p>
+        </nav>
     );
 }
 

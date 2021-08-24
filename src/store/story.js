7a763a5c7d5a7ab5b9a9404a93f64b1d30d1f6c0
Shoozle <sean@seanmurtagh.uk>
2021-08-24 04:19:13 +0100

    Story reducer and flowchart start

---
+++ src/store/story.js
@@ -0,0 +1,14 @@
+const index = { index: 0 };
+
+function indexReducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { index: state.index + 1 };
+        case 'decrement':
+            return { index: state.index - 1 };
+        default:
+            throw new Error();
+    }
+}
+
+export {indexReducer};

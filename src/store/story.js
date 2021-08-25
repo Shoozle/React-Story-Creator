b95513f7b1453c4081e99b6c49d6b5c998a2f257
Shoozle <sean@seanmurtagh.uk>
2021-08-26 00:44:17 +0100

    story reducer

--- src/store/story.js
+++ src/store/story.js
@@ -1,14 +1,27 @@
-const initState = { index: 0 };
 
-function indexReducer(state, action) {
+const storyReducer = (state, action) => {
     switch (action.type) {
-        case 'increment':
-            return { index: state.index + 1 };
-        case 'decrement':
-            return { index: state.index - 1 };
+        case 'UNDO_PAINTING':
+            return { ...state };
+        case 'REDO_PAINTING':
+            return { ...state };
+        case 'ADD_PAINTING':
+            return { ...state };
+        case 'NEXT_PAGE':
+            return { ...state };
+        case 'PREV_PAGE': 
+            return { ...state };
+        case 'ADD_PAGE':
+            const oldPages = [...state.pages]
+            const pageNum = oldPages.length + 1;
+            oldPages.push({description: '', paintingIndex: 0, paintings: []})
+            
+            return { ...state, pageNum, oldPages };
         default:
-            throw new Error();
+            // throw new Error('Unexpected case');
     }
 }
 
-export {indexReducer};
+
+
+export default storyReducer;

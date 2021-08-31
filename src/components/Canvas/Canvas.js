9dc81b069f46ba28504c3caa198aab7666c7ed6f
Shoozle <sean@seanmurtagh.uk>
2021-08-31 11:44:20 +0100

    Separated pagenum to state, payload to reducer

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -3,12 +3,11 @@ import storyReducer from '../../store/story';
 import classes from './canvas.module.css'
 
 const initialStoryState = {
-    pageNum: 0,
     pages: [
         {
             description: '',
-            paintingIndex: 0,
-            paintings: []
+            editIndex: 0,
+            edits: []
         }
     ]
 };
@@ -17,29 +16,27 @@ const Canvas = (props) => {
     
     const canvasRef = useRef(null);
     const [painting, setPainting] = useState(false);
-
-    //State is always most uptodate state
+    const [pageNum, setPageNum] = useState(0);
     const [storyState, dispatchStory] = useReducer(storyReducer, initialStoryState)
-    //Storyreducer is the function that returns a new updated state based on prevstate and action done (prevState, action)
 
-    const { pageNum, pages } = storyState;
-    const index = pages[pageNum].paintingIndex;
+    const { pages } = storyState;
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         
-        if (pages[pageNum].paintings.length === 0) {
+        if (pages[pageNum].edits.length === 0) {
             ctx.fillStyle = 'white';
             ctx.fillRect(0, 0, 400, 400);
-            dispatchStory({ type: 'ADD_PAINTING', painting: canvas.toDataURL() })
+            dispatchStory({ type: 'ADD_PAINTING', payload: { pageNum, edit: canvas.toDataURL() } })
         }
 
-        let image = new Image();
-        image.src = pages[pageNum].paintings[index];
+        const image = new Image();
+        image.src = pages[pageNum].edits[pages[pageNum].editIndex -1];
         image.onload = () => ctx.drawImage(image, 0, 0);
+        console.log('useEffect', pages, pageNum)
 
-    }, [pageNum, pages, index])
+    }, [pages, pageNum])
 
     const startPosition = () => {
         setPainting(true)
@@ -50,7 +47,7 @@ const Canvas = (props) => {
             const canvas = canvasRef.current;
             const ctx = canvas.getContext('2d');
             ctx.beginPath();
-            dispatchStory({ type: 'ADD_PAINTING', painting: canvas.toDataURL() })
+            dispatchStory({ type: 'ADD_PAINTING', payload: { pageNum, edit: canvas.toDataURL() } })
         }
         setPainting(false);
     }
@@ -73,29 +70,30 @@ const Canvas = (props) => {
     }
 
     const undo = () => {
-        if (pages[pageNum].paintingIndex > 0) {
-            dispatchStory({type: 'UNDO_PAINTING'});
+        if (pages[pageNum].editIndex > 0) {
+            dispatchStory({type: 'UNDO_PAINTING', payload: { pageNum }});
         }
     }
 
     const redo = () => {
-        if (pages[pageNum].paintingIndex < pages[pageNum].paintings.length) {
-            dispatchStory({type: 'REDO_PAINTING'});
+        if (pages[pageNum].editIndex < storyState.pages[pageNum].edits.length - 1) {
+            dispatchStory({type: 'REDO_PAINTING', payload: { pageNum }});
         }
     }
 
     const newPage = () => {
-        dispatchStory({ type: 'ADD_PAGE' })
+        dispatchStory({ type: 'ADD_PAGE', payload: { pageNum } })
+        setPageNum(pages.length)
     }
 
     const nextPage = () => {
-        if (pageNum < pages.length -1) 
-            dispatchStory({ type: 'NEXT_PAGE' })
+        if (pageNum < pages.length -1)
+            setPageNum((prevPageNum) => prevPageNum + 1)
     }
 
     const prevPage = () => {
         if (pageNum > 0) 
-        dispatchStory({ type: 'PREV_PAGE' })
+            setPageNum((prevPageNum) => prevPageNum - 1)
     }
 
     return (
@@ -117,7 +115,7 @@ const Canvas = (props) => {
             <button onClick={prevPage}>Previous Page</button>
             <button onClick={newPage}>Add a Page</button>
             <button onClick={nextPage}>Next Page</button>
-            <h1>Page {storyState.pageNum + 1} of {storyState.pages.length}</h1>
+            <h1>Page {pageNum + 1} of {storyState.pages.length}</h1>
         </div>
     )
 }
--- src/store/story.js
+++ src/store/story.js
@@ -2,52 +2,35 @@ import update from 'immutability-helper';
 
 const storyReducer = (state, action) => {
 
-    const { pageNum, pages } = state;
+    const { pages } = state;
+    const { edit, pageNum } = action.payload;
 
     switch (action.type) {
         case 'UNDO_PAINTING':
             {
-                const pageToAlter = { ...pages[pageNum] };
-                pageToAlter.paintingIndex -= 1;
-                pages[pageNum] = pageToAlter;
-                return { pageNum, pages }
+                const oldPages = [...state.pages]
+                oldPages[pageNum].editIndex--;
+                return { pages: oldPages }
             }
         case 'REDO_PAINTING':
             {
-                const pageToAlter = { ...pages[pageNum] };
-                pageToAlter.paintingIndex += 1;
-                pages[pageNum] = pageToAlter;
-                return { pageNum, pages }
+                const oldPages = [...state.pages]
+                oldPages[pageNum].editIndex++;
+                return { pages: oldPages }
             }
         case 'ADD_PAINTING':
             {
-                const oldPage = { ...state.pages[state.pageNum] };
-                const oldPaintings = oldPage.paintings;
-                const newState = oldPaintings.concat(action.painting)
-                const updatedPage = {
-                    description: '',
-                    paintingIndex: newState.length - 1,
-                    paintings: newState
-                }
-                const pages = state.pages[state.pageNum] = updatedPage
+                pages[pageNum].edits.push(edit);
+                pages[pageNum].editIndex++;
                 return { ...state }
-            }
-        case 'NEXT_PAGE':
-            {
-                const newCurrentPage = pageNum + 1;
-                return { ...state, pageNum: newCurrentPage };
-            }
-        case 'PREV_PAGE':
-            {
-                const newCurrentPage = pageNum - 1;
-                return { ...state, pageNum: newCurrentPage };
             }
         case 'ADD_PAGE':
             {
                 const oldPages = [...state.pages]
-                const pageNum = oldPages.length;
-                oldPages.push({ description: '', paintingIndex: 0, paintings: [] })
-                return { pageNum, pages: oldPages };
+                console.log(oldPages)
+                oldPages.push({ description: '', editIndex: 0, edits: [] })
+                console.log(oldPages)
+                return { pages: oldPages };
             }
         default:
             alert('No action for this type')

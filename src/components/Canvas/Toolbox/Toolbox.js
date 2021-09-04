6dc099c676f21fc328b028e3686f24272528f121
Shoozle <sean@seanmurtagh.uk>
2021-09-04 09:03:06 +0100

    colorpicker css, need queries

--- src/components/Canvas/Toolbox/Toolbox.js
+++ src/components/Canvas/Toolbox/Toolbox.js
@@ -1,7 +1,7 @@
 import classes from './toolbox.module.css';
+import './brush.css';
 import Button from '../../UI/Button/Button';
-import { HexColorPicker  } from 'react-colorful';
-import { useState } from 'react';
+import { HexColorPicker } from 'react-colorful';
 
 const Toolbox = (props) => {
 
@@ -9,24 +9,24 @@ const Toolbox = (props) => {
 
     let brushControls;
 
-    if (onBrushSizeChange) { 
+    if (onBrushSizeChange) {
         brushControls = (
             <>
-                <p>Brush Size: </p>
-                <input type="range" value={brushSize} min="1" max="60" onChange={(e) => onBrushSizeChange(e)}></input>
-                <HexColorPicker color={color} onChange={setColor} />
-        </>
+                <label htmlFor="size">Brush Size</label>
+                <input type="range" id="size" value={brushSize} min="1" max="60" onChange={(e) => onBrushSizeChange(e)}></input>
+                <HexColorPicker className={classes.colorful} color={color} onChange={setColor} />
+            </>
         )
     }
 
     return (
         <div className={`${classes.Toolbox} ${props.placement === 'side' ? classes.side : classes.bottom}`}>
-            { onUndo && <Button onClick={onUndo} text="Undo" />}
-            { onFill && <Button onClick={onFill} text="Fill" />}
-            { onRedo && <Button onClick={onRedo} text="Redo" />}
-            { onPrevPage && <Button onClick={onPrevPage} text="Previous Page" />}
-            { onNewPage && <Button onClick={onNewPage} text="Add a Page" />}
-            { onNextPage && <Button onClick={onNextPage} text="Next Page" />}
+            {onUndo && <Button onClick={onUndo} text="Undo" />}
+            {onFill && <Button onClick={onFill} text="Fill" />}
+            {onRedo && <Button onClick={onRedo} text="Redo" />}
+            {onPrevPage && <Button onClick={onPrevPage} text="Previous Page" />}
+            {onNewPage && <Button onClick={onNewPage} text="Add a Page" />}
+            {onNextPage && <Button onClick={onNextPage} text="Next Page" />}
             {brushControls}
         </div>
     )
